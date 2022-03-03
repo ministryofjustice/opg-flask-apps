10833296085cca4e0b8306208a2c1ec487dc455e
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-03-03 16:46:49 +0000

    initial commit to auto-generated repo

---
+++ rest-api/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_compress import Compress
+#from flask_talisman import Talisman
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+app = Flask(__name__)
+xray_recorder.configure(service='Flask Rest API')
+XRayMiddleware(app, xray_recorder)
+
+#csp = {
+#    "default-src": "'self'"
+#}
+
+Compress(app)
+#Talisman(app, content_security_policy=csp, strict_transport_security_max_age=3600)
+
+from app import routes
