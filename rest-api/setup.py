84e49e47b1c94532dfa8cee9d0d4f9b124a61c52
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-05-27 11:07:01 +0100

    fix setup names

--- rest-api/setup.py
+++ rest-api/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='pipelinescriptslib', 
+setup(name='flaskapi', 
         version='1.0', 
-        description='Base OPG flask app',
+        description='Base OPG flask api',
         packages=find_packages())
