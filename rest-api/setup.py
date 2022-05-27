6200a74bc8471df9c32a23ce8eea35eb5c4d0f8e
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-05-27 10:58:40 +0100

    use setup.py

---
+++ rest-api/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='pipelinescriptslib', 
+        version='1.0', 
+        description='Base OPG flask app',
+        packages=find_packages())
