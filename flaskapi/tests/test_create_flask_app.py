ce58d5f4e7153c0eb9f66df006b15379f3a6f549
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-06-20 17:50:32 +0100

    tests using sqlite

--- flaskapi/tests/test_create_flask_app.py
+++ flaskapi/tests/test_create_flask_app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 from opgflaskapi import create_flask_app
+import sqlite3
+db = sqlite3.connect(':memory:')
 
-def test_create_flask_app():
-    api = create_flask_app("bob", "postgres://someurl")
+def test_create_flask_app_with_db():
+    api = create_flask_app("bob", ':memory:')
     assert isinstance(api, Flask)
+
+def test_create_flask_app_without_db():
+    api = create_flask_app("bob", None)
+    assert isinstance(api, Flask)
+
+    # TODO need to call healthcheck route
