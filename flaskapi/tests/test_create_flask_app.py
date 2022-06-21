80a2bfa7bfa51522493478be415fd045f0f15322
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-06-21 14:50:46 +0100

    test healtcheck route

--- flaskapi/tests/test_create_flask_app.py
+++ flaskapi/tests/test_create_flask_app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from opgflaskapi import create_flask_app
 import sqlite3
-db = sqlite3.connect(':memory:')
+db = sqlite3.connect("file::memory:?cache=shared", uri=True)
 
 def test_create_flask_app_with_db():
-    api = create_flask_app("bob", ':memory:')
+    api = create_flask_app("bob", 'sqlite:///:memory:')
     assert isinstance(api, Flask)
+
+    response = api.test_client().get("/healthcheck")
+    assert response.status_code == 200
 
 def test_create_flask_app_without_db():
     api = create_flask_app("bob", None)
     assert isinstance(api, Flask)
 
-    # TODO need to call healthcheck route
+    response = api.test_client().get("/healthcheck")
+    assert response.status_code == 200
