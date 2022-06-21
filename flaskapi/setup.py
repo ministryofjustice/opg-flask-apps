936f2b76a138101024094706364a882b47a4da73
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-06-21 16:40:14 +0100

    add gunicorn as dependency, needed by repos that use this

--- flaskapi/setup.py
+++ flaskapi/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='opgflaskapi', 
         version='1.0', 
         description='base flask api',
-        install_requires = ['aws_xray_sdk', 'flask-sqlalchemy', 'flask', 'psycopg2-binary'],
+        install_requires = ['aws_xray_sdk', 'flask-sqlalchemy', 'flask', 'psycopg2-binary', 'gunicorn'],
         packages=['opgflaskapi'])
