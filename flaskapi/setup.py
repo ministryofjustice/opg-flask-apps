867287577d7873a45c817830fab07c713434eefe
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-07-05 16:36:15 +0100

    spwcifiy sqlalchemy

--- flaskapi/setup.py
+++ flaskapi/setup.py
@@ -6,7 +6,8 @@ setup(
     description="base flask api",
     install_requires=[
         "aws_xray_sdk==2.8.0",
-        "flask==2.1.0",
+        "flask>=2.1.0,<3",
+        "sqlalchemy",
         "psycopg2-binary",
         "gunicorn==20.1.0",
     ],
