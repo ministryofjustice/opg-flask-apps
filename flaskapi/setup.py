4f0fde3b3b1b4a1b425a0a306259398166236407
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-07-06 11:40:05 +0100

    setup versions

--- flaskapi/setup.py
+++ flaskapi/setup.py
@@ -5,11 +5,11 @@ setup(
     version="1.0",
     description="base flask api",
     install_requires=[
-        "aws_xray_sdk==2.8.0",
+        "aws_xray_sdk>=2.8.0,<3",
         "flask>=2.1.0,<3",
         "sqlalchemy>=1.4,<2",
         "psycopg2-binary",
-        "gunicorn==20.1.0",
+        "gunicorn>=20.1.0,<21",
     ],
     packages=["opgflaskapi"],
 )
