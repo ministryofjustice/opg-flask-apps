cddeda1d21709833ee34a5a050d91458d64be6a1
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-05 15:59:44 +0100

    get test working

--- flaskfront/tests/test_create_flask_app.py
+++ flaskfront/tests/test_create_flask_app.py
@@ -6,5 +6,8 @@ def test_create_flask_app():
     app = create_flask_app("bob")
     assert isinstance(app, Flask)
 
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
     response = app.test_client().get("/completed-feedback")
     assert response.status_code == 200
