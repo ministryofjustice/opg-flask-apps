f98ad2ffb72dd349f4ddfe078d4801c5f7ea4f79
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-14 10:32:24 +0100

    add test which takes a loader

--- flaskfront/tests/test_create_flask_app.py
+++ flaskfront/tests/test_create_flask_app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from opgflaskfront import create_flask_app
+from jinja2 import FileSystemLoader
 
 
 def test_create_flask_app():
     app = create_flask_app("bob", force_https=False)
+    assert isinstance(app, Flask)
+
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+    response = app.test_client().get("/healthcheck")
+    assert response.status_code == 200
+
+
+def test_create_flask_app_passing_in_loader():
+    loaders = [FileSystemLoader("."), FileSystemLoader("..")]
+
+    app = create_flask_app("bob", force_https=False, loaders=loaders)
     assert isinstance(app, Flask)
 
     response = app.test_client().get("/")
