72d927c13766037e3d4d690d1563c25c0872542f
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-21 11:33:25 +0100

    add comment

--- flaskfront/tests/test_create_flask_app.py
+++ flaskfront/tests/test_create_flask_app.py
@@ -32,5 +32,6 @@ def test_create_flask_app_passing_in_loader():
     response = app.test_client().get("/assets/govuk-frontend-3.14.0.min.js")
     assert response.status_code == 200
 
+    # ensure we get a working 404 not any breakage or a 500
     response = app.test_client().get("/does-not-exist")
     assert response.status_code == 404
