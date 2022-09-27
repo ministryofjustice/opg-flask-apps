64db8b6a30f34a271a1c98bc2d33c6d7b6483647
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2022-09-27 15:30:59 +0100

    use wget instead of curl as this is already on image

--- flaskfront/build.sh
+++ flaskfront/build.sh
@@ -1,11 +1,11 @@
-curl -L https://github.com/alphagov/govuk-frontend/releases/download/v3.14.0/release-v3.14.0.zip > govuk_frontend.zip
+wget https://github.com/alphagov/govuk-frontend/releases/download/v3.14.0/release-v3.14.0.zip -O govuk_frontend.zip
 rm -rf opgflaskfront/static
 unzip -o govuk_frontend.zip -d opgflaskfront/static
 mv opgflaskfront/static/assets/* opgflaskfront/static
 rm -rf opgflaskfront/static/assets
 rm -rf govuk_frontend.zip
 
-curl -L https://github.com/alphagov/govuk-frontend/archive/v3.14.0.zip > govuk_frontend_source.zip
+wget https://github.com/alphagov/govuk-frontend/archive/v3.14.0.zip -O govuk_frontend_source.zip
 unzip -o govuk_frontend_source.zip -d govuk_frontend_source
 rm -rf govuk_components
 mkdir govuk_components
